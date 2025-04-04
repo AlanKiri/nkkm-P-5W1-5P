27598fc5bb6008dc209122f7d4a80e3888142a0a
Alan <alankiris@gmail.com>
2025-04-04 19:29:06 +0300

    tes

--- dvidesimta paskaita/game.py
+++ dvidesimta paskaita/game.py
@@ -22,9 +22,63 @@ class Game():
         
     def start(self):
         while True:
+            self.check_collisions()
             self.screen.update()
             self.snake.move()
             sleep(0.3)
+            
+    def check_collisions(self):
+        current_x, current_y = self.snake.pos()
+        
+        if self.check_border_collisions(current_x, current_y):
+            self.screen.bye()
+            
+        food_collision_index = self.check_food_collisions(current_x, current_y)
+        
+        if food_collision_index != -1:
+            self.snake.queue += 1
+            self.replace_food(food_collision_index)
+            
+        if self.check_snake_tail_collisions(*self.snake.pos()):
+            self.screen.bye()
+            
+            
+    def check_border_collisions(self, x, y):
+        result = False
+        
+        if x < const.X[0] or x > const.X[1]:
+            result = True
+        elif y < const.Y[0] or y > const.Y[1]:
+            result = True
+        return result
+    
+    def check_food_collisions(self, x, y):
+        for index, food in enumerate(self.food):
+            if (round(x), round(y)) == (round(food.pos()[0]), round(food.pos()[1])):
+                return index
+        return -1
+    
+    def check_snake_tail_collisions(self, x, y):
+        result = False
+
+        for path in self.snake.paths:
+            if (round(x), round(y)) == (round(path.pos()[0]), round(path.pos()[1])):
+                result = True
+        return result
+
+    
+    def check_all_collisions(self, x, y):
+        cords = (x, y)
+        result = False
+        
+        if self.check_border_collisions(*cords):
+            result = True
+        if self.check_food_collisions(*cords) != -1:
+            result = True
+        if  self.check_snake_tail_collisions(*cords):
+            result = True
+            
+        return result
         
     
     def listen_keyboard(self):
@@ -49,6 +103,16 @@ class Game():
             new_food = Food()
             new_food.render()
             self.food.append(new_food)
+            
+    def replace_food(self, index):
+        self.food[index].sabotage()
+        del self.food[index]
+        while True:
+            new_food = Food()
+            if not self.check_all_collisions(*new_food.pos()):
+                new_food.render()
+                self.food.append(new_food)
+                break
         
         
     def init_grid(self):
--- dvidesimta paskaita/snake.py
+++ dvidesimta paskaita/snake.py
@@ -6,12 +6,37 @@ class Snake(Turtle):
         super().__init__(shape='square')
         self.penup()
         self.teleport(const.adapt_grid(0), const.adapt_grid(0))
+        
         self.paths = []
         self.queue = 0
+        self.generate_initial_paths()
+        
         self.color('green')
         self.turtlesize(const.ENTITY_SIZE, const.ENTITY_SIZE)
         
+    def generate_initial_paths(self):
+        for x in range(const.STARTING_SNAKE_SIZE, 0, -1):
+            self.append_path(const.adapt_grid(0), const.adapt_grid(0 - x * const.GRID_SIZE))
+        
+    def append_path(self, x, y):
+        tail = Turtle(shape='square')
+        tail.color('green')
+        tail.turtlesize(const.ENTITY_SIZE-0.5, const.ENTITY_SIZE-0.5)
+        tail.teleport(x,y)
+        self.paths.append(tail)
+        
+    def sabotage_path(self, path:Turtle):
+        path.hideturtle()
+        
     def move(self):
+        self.append_path(*self.pos())
+        if self.queue == 0:
+            path_for_removal_index = 0
+            self.sabotage_path(self.paths[path_for_removal_index])
+            del self.paths[path_for_removal_index]
+        else:
+            self.queue -= 1
+        
         self.forward(const.GRID_SIZE)
         
     def can_rotate(self, move):
